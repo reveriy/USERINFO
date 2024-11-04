254dbcfd6f58cb6cf1b1ba70b054d8f985bf4d98
Valorie Portem <valorieportem@gmail.com>
2024-11-04 17:38:33 +0800

    git commit -m "initial commit"

---
+++ classSubject.js
@@ -0,0 +1,6 @@
+let subjectTitle = "Introduction to Computer Science";
+let classSchedule = "10:30 AM -P12:30 oM, Mondays and Wednesdays";
+let classroom = "Room 204";
+let classInstructor = "Mr. Gacusan";
+let studentName = "Valorie Portem";
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
